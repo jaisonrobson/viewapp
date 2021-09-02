df19549a7575a316e2c3256af4ef7a96dac65f2a
jaisonrobson <xd_jrg@hotmail.com>
2021-09-01 23:42:31 -0300

    implementado repeticao de texto para fins de teste em App.jsx

--- src/views/app/App.jsx
+++ src/views/app/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import LoremIpsum from '../../data/LoremIpsum'
 
-const App = () => <div>
-    <p>
-        <LoremIpsum />
-    </p>
+import _ from 'lodash/fp'
 
-</div>
+const App = () => {
+    const lorem = _.times(() => <p><LoremIpsum /></p>)
+
+    return (
+        <div>
+            {lorem(50)}
+        </div>
+    )
+}
 
 export default App
