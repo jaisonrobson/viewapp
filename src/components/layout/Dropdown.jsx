3a6c5e8841fc7fb7d39bde1ec0285a59bc804570
jaisonrobson <xd_jrg@hotmail.com>
2021-09-05 21:16:24 -0300

    feito funcionamento de abertura automatica do dropdown ao passar o mouse

--- src/components/layout/Dropdown.jsx
+++ src/components/layout/Dropdown.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react'
+import reactDom from 'react-dom'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap'
 
@@ -22,11 +23,28 @@ const Menu = (props) => <DropdownMenu {...props} />
 const Item = (props) => <DropdownItem {...props} />
 
 const Dropdown = (props) => {
+    const ref = useRef(null)
     const { isOpen, setIsOpen } = useContext(FlagContext)
     const toggle = () => setIsOpen(!isOpen)
 
+    useEffect(() => {
+        const over = () => setIsOpen(true)
+        const leave = () => setIsOpen(false)
+
+        const node = reactDom.findDOMNode(ref.current)
+
+        node.addEventListener("mouseover", over)
+        node.addEventListener("mouseleave", leave)
+
+        return () => {
+            node.removeEventListener("mouseover", over)
+            node.removeEventListener("mouseleave", leave)
+        }
+    })
+
     return (
         <UncontrolledDropdown
+            ref={ref}
             isOpen={isOpen}
             toggle={toggle}
             {...props}
