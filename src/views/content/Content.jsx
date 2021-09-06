2bad30e9f17b87c35b1272f52c2206e734e304fe
jaisonrobson <xd_jrg@hotmail.com>
2021-09-05 22:48:44 -0300

    envolvido componente reactstrap em Carousel.jsx & utilizado em App.jsx

--- src/views/content/Content.jsx
+++ src/views/content/Content.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
 
+import Carousel from '../../components/layout/Carousel'
+
 const Content = () => {
     return (
         <Container>
             <Row>
                 <Col>
-
+                    <Carousel />
                 </Col>
             </Row>
         </Container>
