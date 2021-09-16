cf6533b392f652cb8f78e00a8c582d5b6eab1a83
jaisonrobson <xd_jrg@hotmail.com>
2021-09-16 19:19:22 -0300

    alimentado payload de PostersDisplay.jsx

--- src/components/layout/PostersDisplay.jsx
+++ src/components/layout/PostersDisplay.jsx
@@ -3,8 +3,60 @@ import React from 'react'
 import HorizontalScroll from 'components/layout/HorizontalScroll'
 import Cover from 'components/layout/Cover'
 
+import TemplosSecretos from 'images/TemplosSecretos.jpg'
+import ViagemInacreditavel from 'images/ViagemInacreditavel.jpg'
+import EsportesNaMontanha from 'images/EsportesNaMontanha.jpg'
+import Flamingo from 'images/Flamingo.jpg'
+import LuaNova from 'images/LuaNova.jpg'
+import MagoFantasia from 'images/MagoFantasia.jpg'
+import Nave from 'images/Nave.jpg'
+import Robo from 'images/Robo.jpg'
+
+const payload = [
+    {
+        src: TemplosSecretos,
+        genre: 'Nature',
+        title: 'Secret Temples',
+    },
+    {
+        src: EsportesNaMontanha,
+        genre: 'Action',
+        title: 'Mountain King',
+    },
+    {
+        src: Flamingo,
+        genre: 'Nature',
+        title: 'Beautiful in everything',
+    },
+    {
+        src: Nave,
+        genre: 'Scifi',
+        title: 'Into the Space',
+    },
+    {
+        src: LuaNova,
+        genre: 'Horror',
+        title: 'After Midnight',
+    },
+    {
+        src: MagoFantasia,
+        genre: 'Fantasy',
+        title: 'Beyond the Valley',
+    },
+    {
+        src: Robo,
+        genre: 'Scifi',
+        title: 'Fight to Death',
+    },
+    {
+        src: ViagemInacreditavel,
+        genre: 'Adventure',
+        title: 'Unbelievable Trip',
+    }
+]
+
 const PostersDisplay = () => (
-    <HorizontalScroll>
+    <HorizontalScroll items={payload}>
         {({ src, genre, title }) =>
             <Cover
                 image={src}
