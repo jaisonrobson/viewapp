378f82d0f782ef194dd484f2e3d8bd0b15ad1a02
jaisonrobson <xd_jrg@hotmail.com>
2021-09-17 16:36:08 -0300

    adicionado novas parametrizacoes no css de Row.jsx

--- src/components/layout/Row.jsx
+++ src/components/layout/Row.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { Row as ReactstrapRow } from 'reactstrap'
 
-const StyledRow = styled(({ padding, margin, boxShadow, ...props }) => <ReactstrapRow {...props} />)`
+const StyledRow = styled(({ padding, margin, marginTop, marginBottom, boxShadow, ...props }) => <ReactstrapRow {...props} />)`
     ${({ padding }) => padding ? `padding: ${padding};` : ``}
     ${({ margin }) => margin ? `margin: ${margin};` : ``}
+    ${({ marginTop }) => marginTop ? `margin-top: ${marginTop};` : ``}
+    ${({ marginBottom }) => marginBottom ? `margin-bottom: ${marginBottom};` : ``}
     ${({ boxShadow }) => boxShadow ? `box-shadow: ${boxShadow};` : ``}
 `
 
