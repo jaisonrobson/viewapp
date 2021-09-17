c137490a8a4f35826a9c9f48d4fd81ce3982365f
jaisonrobson <xd_jrg@hotmail.com>
2021-09-17 15:06:55 -0300

    adicionado fundo de sobreposicao escuro no Ribbon.jsx

--- src/components/layout/Ribbon.jsx
+++ src/components/layout/Ribbon.jsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 
 import ViagemInacreditavel from 'images/ViagemInacreditavel.jpg'
 
-const StyledWrapper = styled(({ backgroundImage, ...props }) => <div {...props} />)`
+const StyledWrapper = styled.div`
+    position: relative;
+`
+
+const StyledRibbon = styled(({ backgroundImage, ...props }) => <div {...props} />)`
     background-image: url(${({ backgroundImage }) => backgroundImage});
     -webkit-background-size: 200%;
     background-size: 200%;
@@ -14,9 +18,22 @@ const StyledWrapper = styled(({ backgroundImage, ...props }) => <div {...props} 
     height: 600px;
 `
 
+const StyledOverlay = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: linear-gradient(to bottom,rgba(0,0,0,.5) 10%,rgba(0,0,0,.3) 100%);
+`
+
 const Ribbon = ({ image = ViagemInacreditavel, ...props }) => {
     return (
-        <StyledWrapper backgroundImage={image} {...props} />
+        <StyledWrapper>
+            <StyledOverlay />
+
+            <StyledRibbon backgroundImage={image} {...props} />
+        </StyledWrapper>
     )
 }
 
