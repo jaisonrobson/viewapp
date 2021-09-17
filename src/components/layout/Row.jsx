b68ab0e4c6e555ed74ea9893f13f7e41fd1a4976
jaisonrobson <xd_jrg@hotmail.com>
2021-09-17 15:05:43 -0300

    adicionado parametrizacao de prop boxShadow no estilo css de Row.jsx

--- src/components/layout/Row.jsx
+++ src/components/layout/Row.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import { Row as ReactstrapRow } from 'reactstrap'
 
-const StyledRow = styled(({ padding, margin, ...props }) => <ReactstrapRow {...props} />)`
+const StyledRow = styled(({ padding, margin, boxShadow, ...props }) => <ReactstrapRow {...props} />)`
     ${({ padding }) => padding ? `padding: ${padding};` : ``}
     ${({ margin }) => margin ? `margin: ${margin};` : ``}
+    ${({ boxShadow }) => boxShadow ? `box-shadow: ${boxShadow};` : ``}
 `
 
 const Row = (props) => <StyledRow {...props} />
