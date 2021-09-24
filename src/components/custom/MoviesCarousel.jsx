78546a9da05270bb0dd71fe40fefcec5a6000f80
jaisonrobson <xd_jrg@hotmail.com>
2021-09-24 20:08:51 -0300

    refatorado MoviesCarousel

--- src/components/custom/MoviesCarousel.jsx
+++ src/components/custom/MoviesCarousel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Col from 'components/layout/Col'
 import Container from 'components/layout/Container'
 import Row from 'components/layout/Row'
 import Carousel from 'components/layout/Carousel'
+import Button from 'components/layout/Button'
 
 const initialPayload = [
     {
@@ -16,37 +18,56 @@ const initialPayload = [
     }
 ]
 
-const MoviesCarousel = ({ payload = initialPayload }) => {
-    return (
-        <Carousel
-            payload={payload}
-            imageParameter="imageSrc"
-            caption={({ payload, active }) => (
-                <div />
-                // <Container>
-                //     <Row>
-                //         <Col>
-                //             <h1 style={{ color: '#fff' }}>
-                //                 {payload[active].caption}
-                //             </h1>
-                //             <p
-                //                 style={{
-                //                     fontFamily: "'Roboto',sans-serif",
-                //                     fontSize: '12px',
-                //                     lineHeight: '1.5em',
-                //                     color: '#FFFFFFA8',
-                //                     textShadow: '1px 1px 2px #57575770',
-                //                 }}
-                //             >
-                //                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                //             </p>
-                //             <button>Watch</button>
-                //         </Col>
-                //     </Row>
-                // </Container>
-            )}
-        />
-    )
-}
+const StyledH1 = styled.h1`
+    color: #ffffff;
+`
+
+const StyledP = styled.p`
+    font-family: 'Roboto',sans-serif;
+    font-size: 12px;
+    line-height: 1.5em;
+    color: #FFFFFFA8;
+    text-shadow: 1px 1px 2px #57575770;
+`
+
+const MoviesCarousel = ({ payload = initialPayload }) => (
+    <Carousel
+        payload={payload}
+        imageParameter="imageSrc"
+        caption={({ payload, active }) => (
+            <Container>
+                <Row>
+                    <Col>
+                        <StyledH1 className="unselectable">
+                            {payload[active].title}
+                        </StyledH1>
+                    </Col>
+                </Row>
+
+                <Row marginTop="1rem">
+                    <Col>
+                        <div className="text-gray-400 unselectable" style={{ display: 'inline', marginRight: '2rem', paddingRight: '2rem', borderRight: '1px solid' }}>{payload[active].year}</div>
+                        <div className="text-gray-400 unselectable" style={{ display: 'inline', marginRight: '2rem', paddingRight: '2rem', borderRight: '1px solid' }}>{payload[active].duration} min</div>
+                        <div className="text-gray-400 unselectable" style={{ display: 'inline' }}>{payload[active].genre}</div>
+                    </Col>
+                </Row>
+
+                <Row marginTop="1rem">
+                    <Col>
+                        <StyledP className="unselectable">
+                            {payload[active].synopsis}
+                        </StyledP>
+                    </Col>
+                </Row>
+
+                <Row marginTop="1rem">
+                    <Col>
+                        <Button>Watch</Button>
+                    </Col>
+                </Row>
+            </Container>
+        )}
+    />
+)
 
 export default MoviesCarousel
