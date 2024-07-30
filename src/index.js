8ce28f6b052950d4da9930ecffbcf884c3c00c8b
jaisonrobson <xd_jrg@hotmail.com>
2024-07-30 15:57:39 -0300

    build: adicionado lib `react-router` & feito configuracao inicial do `react-router` no projeto incluindo componente `App` como rota principal

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import './css/bootstrap.css'
 import 'index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { createRoot } from 'react-dom/client';
-import App from './views/app/App'
+import App from './routes/app/App'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        //errorElement: ,
+        //loader: ,
+        //action: ,
+        //children: [],
+    },
+])
+
+root.render(<RouterProvider router={router} />)
